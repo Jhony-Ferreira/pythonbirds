090c8d19ad0fe3bf9d448540edbbbd32a5e3c260
Jhony Ferreira <jhonytavares@yahoo.com.br>
2022-08-24 22:45:51 -0300

    Implementado Carro com composição de Motor e Direcao

--- oo/carro.py
+++ oo/carro.py
@@ -73,16 +73,40 @@ O   L
     'Leste'
     >>> carro.girar_a_direita()
     >>> carro.calcular_direcao()
-    'Norte'
+    'Sul'
     >>> carro.girar_a_esquerda()
     >>> carro.calcular_direcao()
-    'Oeste'
+    'Leste'
 """
 
 NORTE='Norte'
 SUL='Sul'
 LESTE='Leste'
 OESTE='Oeste'
+
+
+class Carro:
+    def __init__(self, direcao, motor):
+        self.motor = motor
+        self.direcao = direcao
+
+    def calcular_velocidade(self):
+        return self.motor.velocidade
+
+    def acelerar(self):
+        return self.motor.acelerar()
+
+    def frear(self):
+        return self.motor.frear()
+
+    def calcular_direcao(self):
+        return self.direcao.valor
+
+    def girar_a_direita(self):
+        return self.direcao.girar_a_direita()
+
+    def girar_a_esquerda(self):
+        return self.direcao.girar_a_esquerda()
 
 class Direcao:
     rotacao_a_direita_dct = {
@@ -101,8 +125,6 @@ class Direcao:
 
     def girar_a_esquerda(self):
         self.valor = self.rotacao_a_esquerda_dct[self.valor]
-
-
 
 
 class Motor:

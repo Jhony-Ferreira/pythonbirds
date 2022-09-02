538724a6b3373573972cd6b386bb598c034444f0
Jhony Ferreira <jhonytavares@yahoo.com.br>
2022-09-02 16:58:46 -0300

    Implementado o restante dos testes de atores, fase e integração assim como todos ajustes necessarios para o tutorial basico

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -7,7 +7,7 @@ class Pessoa:
         self.filhos = list(filhos)
 
     def cumprimentar(self):
-        return f'Olá {id(self)}'
+        return f'Olá meu nome é {self.nome}'
 
     @staticmethod
     def metodo_estatico():
@@ -17,10 +17,14 @@ class Pessoa:
     def nome_e_atributos_de_clsse(cls):
         return f'{cls} - olhos: {cls.olhos}'
 
+class Homem(Pessoa):
+    def cumprimentar(self):
+        cumprimentar_da_clase = super().cumprimentar()
+        return f'{cumprimentar_da_clase}. Aperto de mão'
 
 if __name__ == '__main__':
-    jhony = Pessoa(nome='Jhony')
-    zegrilo = Pessoa(jhony, nome='zegrilo')
+    jhony = Homem(nome='Jhony')
+    zegrilo = Homem(jhony, nome='zegrilo')
     print(Pessoa.cumprimentar(zegrilo))
     print(id(zegrilo))
     print(zegrilo.cumprimentar())
@@ -41,3 +45,8 @@ if __name__ == '__main__':
     print(zegrilo.olhos)
     print(Pessoa.metodo_estatico(), zegrilo.metodo_estatico())
     print(Pessoa.nome_e_atributos_de_clsse(), zegrilo.nome_e_atributos_de_clsse())
+    pessoa = Pessoa('Anonimo')
+    print(isinstance(pessoa, Pessoa))
+    print(isinstance(pessoa, Homem))
+    print(isinstance(jhony, Pessoa))
+    print(isinstance(jhony, Homem))
